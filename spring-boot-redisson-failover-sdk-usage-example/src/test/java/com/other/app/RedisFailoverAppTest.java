a0f7e62d1480720489d273be6178c3110f02ab6e
huqianghui <huqianghui0613@163.com>
2021-06-24 09:59:59 +0800

    Update RedisFailoverAppTest.java

--- spring-boot-redisson-failover-sdk-usage-example/src/test/java/com/other/app/RedisFailoverAppTest.java
+++ spring-boot-redisson-failover-sdk-usage-example/src/test/java/com/other/app/RedisFailoverAppTest.java
@@ -1,9 +1,12 @@
 package com.other.app;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class RedisFailoverAppTest {
 
     @Test
